50a2ff67766078dd099e647d4d4167133c763b9a
Steam Deck User <deck@steamdeck.home>
2024-06-27 22:25:25 +0300

    Working on sockets

--- websocket/client/src/main.zig
+++ websocket/client/src/main.zig
@@ -4,26 +4,15 @@ const net = std.net;
 pub fn main() !void {
     const address = net.Address.initIp4(.{ 127, 0, 0, 1 }, 5101);
     const conn = try net.tcpConnectToAddress(address);
+    //defer std.net.Stream.close(conn);
     defer conn.close();
 
-    _ = try conn.write("Hello world \n");
+    for (0..16) |_| {
+        _ = try conn.write(" Hello world");
+        std.time.sleep(1_000_000);
+        // var buf: [256]u8 = undefined;
+        // _ = try conn.read(buf[0..]);
+    }
 
-    var buf: [256]u8 = undefined;
-    _ = try conn.read(buf[0..]);
+    while (true) {}
 }
-
-// fn s () void {
-//     const address = net.Address.initIp4(.{ 127, 0, 0, 1 }, 5101);
-//     var server = address.getOsSockLen();
-//     defer server.deinit();
-
-//     var client = try server.accept();
-
-//     _ = client.stream.write("Hope this works\n") catch |err| switch (err) {
-//         else => {
-//             std.debug.print("Failed {}", .{err});
-//             return;
-//         },
-//     };
-//     std.debug.print("Sent {d}", .{address.getPort()});
-// }
--- websocket/server/src/main.zig
+++ websocket/server/src/main.zig
@@ -10,20 +10,45 @@ pub fn main() !void {
 fn loop() !void {
     const address = net.Address.initIp4(.{ 127, 0, 0, 1 }, 5101);
 
-    var buf: [256]u8 = undefined;
     var server = try address.listen(.{});
 
     defer server.deinit();
 
     while (true) {
         const conn = try server.accept();
+        errdefer conn.stream.close();
 
-        const bytes = try conn.stream.read(&buf);
-        std.debug.print("[INFO] Received {d} bytes from client - {s}\n", .{ bytes, buf });
+        try connectedClient(conn);
+    }
+}
+
+fn connectedClient(conn: net.Server.Connection) !void {
+    var buf: [256]u8 = undefined;
+    while (true) {
+        const bytes = conn.stream.read(&buf) catch |err| switch (err) {
+            else => {
+                std.debug.print("Error caught: {}", .{err});
+                conn.stream.close();
+                break;
+            },
+        };
+
+        if (bytes == 0) {
+            //std.debug.print("Still trying to read", .{});
+            _ = conn.stream.write("\n") catch |err| switch (err) {
+                else => {
+                    std.debug.print("Error caught: {}\n", .{err});
+                    conn.stream.close();
+                    break;
+                },
+            };
+            continue;
+        }
+        std.debug.print("[INFO] Received {d} bytes from client - {s}\n", .{ bytes, buf[0..bytes] });
         //conn.stream.write("");
 
-        conn.stream.close();
-    }
+        //conn.stream.close();
+    } else unreachable;
 }
 
 // WHAT IS A HASHMAP
